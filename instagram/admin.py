6564e7f4fdfc4578b6c57d96afc3f918a014cad7
every3743 <every3743@gmail.com>
2021-12-21 11:44:20 +0900

    커밋

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+# admin.site.register(Post)
+
+@admin.register(Post) # Mapping
+# admin.site.register(Post, PostAdmin)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'message_length', 'is_public','created_at', 'updated_at']
+    list_display_links = ['message']
+    list_filter = ['created_at']
+    search_fields = ['message']
+    def message_length(self, post):
+        return f"{len(post.message)} 글자"
