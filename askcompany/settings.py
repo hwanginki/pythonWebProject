850a425a0c5be8c15d7d9e8f3045b638c86e6d7b
every3743 <every3743@gmail.com>
2021-12-22 13:35:35 +0900

    이미지 추가 커밋

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+ 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- instagram/admin.py
+++ instagram/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post
 
 # Register your models here.
@@ -7,9 +8,15 @@ from .models import Post
 @admin.register(Post) # Mapping
 # admin.site.register(Post, PostAdmin)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'message', 'message_length', 'is_public','created_at', 'updated_at']
+    list_display = ['id', 'photo_tag', 'message', 'message_length', 'is_public','created_at', 'updated_at']
     list_display_links = ['message']
     list_filter = ['created_at']
     search_fields = ['message']
+
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}"style="width: 72px;" />')
+        return None
+
     def message_length(self, post):
         return f"{len(post.message)} 글자"
