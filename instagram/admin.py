d9b6498f2ebea23d321512fa21693969ce36765f
every3743 <every3743@gmail.com>
2022-01-02 10:38:10 +0900

    마이그레이션 수정

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,22 +1,4 @@
 from django.contrib import admin
-from django.utils.safestring import mark_safe
 from .models import Post
 
-# Register your models here.
-# admin.site.register(Post)
-
-@admin.register(Post) # Mapping
-# admin.site.register(Post, PostAdmin)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'photo_tag', 'message', 'message_length', 'is_public','created_at', 'updated_at']
-    list_display_links = ['message']
-    list_filter = ['created_at']
-    search_fields = ['message']
-
-    def photo_tag(self, post):
-        if post.photo:
-            return mark_safe(f'<img src="{post.photo.url}"style="width: 72px;" />')
-        return None
-
-    def message_length(self, post):
-        return f"{len(post.message)} 글자"
+admin.site.register(Post)
--- instagram/models.py
+++ instagram/models.py
@@ -1,22 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class Post(models.Model):
     message = models.TextField()
-    photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d')
-    is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # Java의 toString() 메서드 비슷한 가진 str(...)
-    def __str__(self):
-        # return f"Custom Post object ({self.id})"
-        # return "Custom Post object ({})".format(self.id)
-        return self.message
-
-    def message_length(self):
-        return len(self.message)
-    message_length.short_description = "메시지 글자수"
-
-    class Meta:
-        ordering = ['-id']
+def __str__(self):
+    return self.message
